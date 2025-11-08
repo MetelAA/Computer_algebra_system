36804999b959b62580b1394e63341f0fd6d23300
MetelAA <artemmetelski78@gmail.com>
2025-11-08 22:27:13 +0300

    Z9-Z10 added

--- IntegerNumber.cpp
+++ IntegerNumber.cpp
@@ -52,6 +52,52 @@ IntegerNumber IntegerNumber::multiply(const IntegerNumber &other) const {
     bool resultIsNegative = numberSign != otherSign;
 
     return IntegerNumber(multiplyAbs.getNumbers(), resultIsNegative);
-};
+}
 
+//Z9: Частное от деления целого на целое (делитель отличен от нуля)
+IntegerNumber IntegerNumber::quotient(const IntegerNumber &other) const {
+    if (other.getSign() == 0) {
+        throw UniversalStringException("you cannot divide by zero");
+    }
 
+    NaturalNumber dividend = this->abs();
+    const NaturalNumber divisor = other.abs();
+
+    if (dividend.cmp(&divisor) == 1) { // divisor > dividend => поделить не можем, вернем 0
+        return IntegerNumber(std::vector<uint8_t>{0}, false);
+    }
+
+    IntegerNumber quotient(std::vector<uint8_t>{0}, false); // делаем нулевой контейнер для подсчета ответа
+
+    while (dividend.cmp(&divisor) % 2 == 0) { // пока так, позже прикручу бинпоиск, будет log n
+        dividend = dividend.subtract(divisor);
+        quotient = quotient.add(IntegerNumber(std::vector<uint8_t>{1}, false));
+    }
+
+    return this->getSign() != other.getSign() ? quotient.negate() : quotient;
+}
+
+//Z10: Остаток от деления целого на целое(делитель отличен от нуля)
+IntegerNumber IntegerNumber::remainder(const IntegerNumber &other) const {
+    if (other.getSign() == 0) {
+        throw UniversalStringException("you cannot divide by zero");
+    }
+
+    const NaturalNumber dividend_abs = this->abs();
+    const NaturalNumber divisor_abs = other.abs();
+
+    if (dividend_abs.cmp(&divisor_abs) == 1) {
+        return *this;
+    }
+
+    IntegerNumber dividend(*this);
+    IntegerNumber divisor(other);
+
+    if (dividend.getSign() == 1) dividend = dividend.negate();
+    if (divisor.getSign() == 1) divisor = divisor.negate();
+
+    const IntegerNumber quotient = dividend.quotient(divisor);
+    const IntegerNumber product = divisor.multiply(quotient);
+
+    return IntegerNumber(dividend.subtract(product).getNumbers(), this->getSign());;
+}
